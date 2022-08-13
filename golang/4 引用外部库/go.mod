216a440545d84a0911d8f73198eb5bef04388816
langwanluo <langwnaluo@gmail.com>
2022-08-13 17:28:56 +0800

    4

---
+++ golang/4 引用外部库/go.mod
@@ -0,0 +1,5 @@
+module app
+
+go 1.16
+
+require github.com/satori/go.uuid v1.2.0 // indirect
---
+++ golang/4 引用外部库/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func main() {
+	u1 := uuid.NewV4()
+	fmt.Printf("UUIDv4: %s\n", u1)
+}
