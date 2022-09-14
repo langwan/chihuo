3c546aee0870581900471b06e3e5ff0f22c24e63
langwanluo <langwnaluo@gmail.com>
2022-09-14 17:18:08 +0800

    修正 发布组件的代码

--- golang/golang基础教程/30 发布组件/go.mod
+++ golang/golang基础教程/30 发布组件/go.mod
@@ -1,3 +1,5 @@
 module app
 
 go 1.17
+
+require github.com/langwan/go-jwt-hs256 v1.0.0 // indirect
--- golang/golang基础教程/30 发布组件/main.go
+++ golang/golang基础教程/30 发布组件/main.go
@@ -1,1 +1,23 @@
-package _0_发布组件
+package main
+
+import (
+	"fmt"
+	"github.com/langwan/go-jwt-hs256"
+)
+
+func main() {
+
+	jwt.Secret = "123456"
+	payload := struct {
+		Name string `json:"name"`
+	}{Name: "chihuo"}
+	sign, err := jwt.Sign(payload)
+	if err != nil {
+		return
+	}
+	err = jwt.Verify(sign)
+	if err != nil {
+		return
+	}
+	fmt.Println("ok")
+}
